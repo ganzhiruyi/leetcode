29621b243e2c62a8f3e67431f591bb6e92348f41
ganzhiruyi <ganzhiruyi0@gmail.com>
2013-09-14 18:16:14 +0800

    Create Swap Nodes in Pairs.cpp

---
+++ Swap Nodes in Pairs.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *swapPairs(ListNode *head) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        ListNode *pre,*cur,*post;
+        cur = head,pre = NULL,post;
+        bool flag = true;
+        while(cur != NULL){
+            post = cur->next;
+            if(post != NULL){
+                if(flag){
+                    head = post;
+                    flag = false;
+                }
+                cur->next = post->next;
+                post->next = cur;
+                if(pre != NULL) pre->next = post;
+                pre = cur;
+                cur = cur->next;
+            }
+            else break;
+        }
+        return head;
+    }
+};
